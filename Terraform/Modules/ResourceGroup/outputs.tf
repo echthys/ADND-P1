49f79530412e5018a01e706fdd543bc651ce7421
EC <ec@dlmail.co.uk>
2022-06-11 01:51:10 +0100

    Created Resource Group Module Used to create resource group in Azure

---
+++ Terraform/Modules/ResourceGroup/outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "The name of the created resource group."
+  value       = var.name
+}
+
+output "location" {
+  description = "The region of the created resource group."
+  value       = var.location
+}
---
+++ Terraform/Modules/ResourceGroup/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type        = string
+  description = "The region the resource group will be created in."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource group to be created."
+}
